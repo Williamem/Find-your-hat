6e6159a5aaeb6e1671872afd4d654bbe1e579d37
WilliamEm <william.emanuelsson@gmail.com>
2023-11-08 14:23:33 +0800

    add Field class

--- main.js
+++ main.js
@@ -4,3 +4,22 @@ const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
+
+
+class Field {
+    constructor(array) {
+        this.field = array;
+    }
+
+/*     printField() {
+        console.log(this.field)
+    } */
+}
+
+/* const myField = new Field([
+    ['*', '░', 'O'],
+    ['░', 'O', '░'],
+    ['░', '^', '░'],
+  ]);
+  
+myField.printField() */
